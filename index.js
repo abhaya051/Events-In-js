7e3bf591b69c4929887c2a5d2e12ce654d8dac17
Udit Kumar <surajgautam783792@gmail.com>
2024-02-25 21:59:19 +0530

    Events

---
+++ index.js
@@ -0,0 +1,56 @@
+// Events
+
+// Events Handling In Js--->>>>>>>>>>>>>
+// How To Write A Event-----------------------------------
+// node.event = ()=>
+// {
+// Handle Here
+// }
+// ----------------------------------------------------------------------------------
+// let btn = document.querySelector("button");
+// btn.onmouseover = () => {
+//   console.log("Button Was Clicked", (document.body.style.background = "red"));
+// };
+
+// btn.onmouseout = () => {
+//   console.log("Button Was Clicked", (document.body.style.background = "white"));
+// };
+
+// Methods E.target E.type E.clientX E.clientY
+
+// let btn = document.querySelector("button");
+// btn.onclick = (e) => {
+//   console.log(e.target);
+// };
+
+// Event Listener
+// let btn = document.querySelector("button");
+
+// // Add Event Listener Methods
+// btn.addEventListener("click", () => {
+//   console.log((document.body.style.background = "pink"));
+// });
+
+// btn.addEventListener("mouseover", () => {
+//   console.log((document.body.style.background = "yellow"));
+// });
+
+// Remove Event Listener
+// let btn = document.querySelector("button");
+
+// // Add Event Listener Methods
+// btn.addEventListener("click", () => {
+//   console.log("Button Was Clicked -handler1");
+// });
+
+// btn.addEventListener("click", () => {
+//   console.log("Button Was Clicked -handler2");
+// });
+
+// btn.addEventListener("click", () => {
+//   console.log("Button Was Clicked -handler3");
+// });
+
+// btn.addEventListener("click", () => {
+//   console.log("Button Was Clicked -handler4");
+// });
